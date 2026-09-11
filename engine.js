66c2f614a4304c9d8fabc62f433b812d48e1f3ff
Mayrik-P <mayrikpadey@gmail.com>
2026-09-11 22:55:25 +0200

    Add files via upload

--- engine.js
+++ engine.js
@@ -3205,6 +3205,8 @@ module.exports = {
   advanceTurn,
   rollMovementDie,
   getForwardDelta,
+  getBackwardDelta,
+  getDirectionDelta,
   computeAiStepCost,
   isAiHiddenHazard,
   findAiAirstrikePlacement,
